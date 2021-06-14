bdf853884a5e3fbf53d9fcdf04be3e8e1770dd8a
Thanh-Truong <hopchuathurac@gmail.com>
2021-06-15 00:12:02 +0200

    upgrade pip

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ python3 -m venv env
 source env/bin/activate
 # Install packages
 export TRUSTED_SITES="--trusted-host pypi.org --trusted-host files.pythonhosted.org"
-pip install pyapacheatlas ${TRUSTED_SITES}
+pip install --upgrade pip ${TRUSTED_SITES}
+pip install pyapacheatlas==0.6.0 ${TRUSTED_SITES}
 pip install pylint ${TRUSTED_SITES}
-pip install azure-mgmt-resource ${TRUSTED_SITES}
-pip install azure-mgmt-purview ${TRUSTED_SITES}
-pip install azure-identity ${TRUSTED_SITES}
+pip install azure-identity==1.6.0 ${TRUSTED_SITES}
+pip install azure-mgmt-resource==18.0.0 ${TRUSTED_SITES}
+pip install azure-mgmt-purview==1.0.0b1 ${TRUSTED_SITES}
