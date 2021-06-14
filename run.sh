3874343a6f1946d7b143886f959fb87516c77002
Thanh-Truong <hopchuathurac@gmail.com>
2021-06-14 21:27:56 +0200

    added steps

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ source env/bin/activate
 python purview.py --create-purview
 python purview.py --assign-roles
 python purview.py --import-term-templates term_templates.json
+python purview.py --import-terms terms_system_templates.csv
+# Terms based on custom templates
+python purview.py --import-terms terms.csv
 python purview.py --delete-purview
