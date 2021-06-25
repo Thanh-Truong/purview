ed1a35031c12dab75b4a979046b03fa549ea5f44
Thanh-Truong <hopchuathurac@gmail.com>
2021-06-25 12:18:20 +0200

    bump version 0.0.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from os import path
 
-__version__ = "0.0.3"
+__version__ = "0.0.6"
 # Read the contents of README.md
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
